eb5572fbc40444c0d74f4665539a21a18983d2d0
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-04-14 16:18:36 +0530

    cart screen progress

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import * as Font from "expo-font";
+import { useState } from "react";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// font
+import AppLoading from "expo-app-loading";
+
+import ShopNavigation from "./navigation/ShopNavigation";
+import productsReducer from "./store/reducers/products.reducer";
+import CartReducer from "./store/reducers/cart.reducers";
+
+const reducers = combineReducers({
+  products: productsReducer,
+  cart: CartReducer,
+});
+
+const store = createStore(reducers, composeWithDevTools());
+
+const fetchFonts = async () => {
+  return await Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFondLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFondLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <ShopNavigation />
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
